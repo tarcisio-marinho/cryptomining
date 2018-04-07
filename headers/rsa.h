a905608ccf6b0f713e2af70421eb3dc52d5604a0
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-04-07 02:23:05 -0300

    adding rsa implementation

--- headers/rsa.h
+++ headers/rsa.h
@@ -4,14 +4,20 @@
 #include<vector>
 #include<string>
 
-int totient(int n);
-int prime(int n);
-int generate_e(int n);
-int generate_prime();
-int mod(int x, int y);
-int private_key(int tot, int e);
-std::vector<int> chy(std::string, int e, int n);
-std::string unchy(std::string, int n, int d);
+class RSA{
+public:
+    int totient(int n);
+    bool prime(int n);
+    int generate_e(int n);
+    int mdc(int x, int y);
+    int generate_prime();
+    int mod(int x, int y);
+    int private_key(int tot, int e);
+    std::vector<int> chy(std::string, int e, int n);
+    std::string unchy(std::string, int n, int d);
+private:
+
+}
 
 
 #endif
--- src/rsa.cpp
+++ src/rsa.cpp
@@ -1,40 +1,81 @@
 #include "../headers/rsa.h"
 
-int totient(int n){
+int RSA::totient(int n){
+    if(prime(n)){
+        return n - 1;
+    }
+}
+
+
+bool RSA::prime(int n){
+    int i = 1, times = 0;
+
+    while(i <= n){
+        if(n%i == 0){
+            times++;
+        }
+        i++;
+    }
+
+    if(times == 2){
+        return true;
+    }
+    return false;
+}
+
+
+int RSA::generate_e(int n){
+    while(true){
+        int e = RANDOM(2, LIMITE);
+        if(this->mdc(n, e) == 1){
+            return e;
+        }
+    }
+}
+
+int RSA::mdc(int x, int y){
+    int rest = 1;
+    while(y != 0){
+        rest = x%y;
+        x = y;
+        y = rest;
+    }
+    return x;
+}
+
+
+int RSA::generate_prime(){
+    while(true){
+        int x = RANDOM(1, 100); // 2^2048 standart
+        if(prime(x)){
+            return x;
+        }
+    }
+}
+
+
+int RSA::mod(int x, int y){
+    if(x < y){
+        return x;
+    }
+    return x%y;
+}
+
+
+int RSA::private_key(int tot, int e){
+    int d = 0;
+    while(mod(d*e, tot) != 1){
+        d++;
+    }
+    return d;
+}
+
+
+std::vector<int> RSA::chy(std::string, int e, int n){
     
 }
 
 
-int prime(int n){
-    
-}
-
-
-int generate_e(int n){
-    
-}
-
-
-int generate_prime(){
-    
-}
-
-
-int mod(int x, int y){
-    
-}
-
-
-int private_key(int tot, int e){
-    
-}
-
-
-std::vector<int> chy(std::string, int e, int n){
-    
-}
-
-
-std::string unchy(std::string, int n, int d){
+std::string RSA::unchy(std::string, int n, int d){
     
 }
