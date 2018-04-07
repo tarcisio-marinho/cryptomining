3b75762c0a8e73ec3da300bc3ecce849b1974ef7
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-04-07 01:16:52 -0300

    adding basic stuff

---
+++ src/includes.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDES
+#define INCLUDES
+
+
+#include<unistd.h>
+
+/* Socket interface */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+
+#include <iostream>
+#include "includes.h"
+
+
+int main(int argc, char *argv[]){
+
+
+}
