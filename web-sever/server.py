196d80f2d3a305846e87747d94e0603340c544d8
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-07-03 17:00:08 -0300

    adding python web server

---
+++ web-sever/server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+from sys import argv
+
+class S(BaseHTTPRequestHandler):
+
+    def send_pool_info(self):
+        pool_name = "teste2"
+        pool_key = "teste"
+        return pool_key, pool_name
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        pool_info = json.dumps(self.send_pool_info())
+        self.wfile.write(pool_info)
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting web server...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
