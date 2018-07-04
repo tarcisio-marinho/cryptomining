1296ceb378330fa770781bf041ddb16691c8d0c5
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-07-04 18:09:38 -0300

    communication update

--- src/communication.cpp
+++ src/communication.cpp
@@ -1,20 +1,10 @@
 #include "communication.h"
 
-
-void Communication::connect_forever(){
-}
-
-
-void Communication::listen_forever(){
-   
-}
-
-
-char * Communication::recv_message(int sock){
-
-}
-
-
-void Communication::send_message(int sock, const char * message){
+Communication::Communication(){
     
 }
+
+void Communication::get_server_info(){
+
+}
+
--- src/communication.h
+++ src/communication.h
@@ -16,26 +16,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-/* Defines */
-#define PORT 8080
-#define size 512
-#define IP "127.0.0.1"
-#define MAX_TERMINAL_OUTPUT 50000
-#define MAX_CONNECTIONS 20
-#define err -1
-
 class Communication{
 
 public:
     /* Client functions */
-    Communication(char *, int);
-    void connect_forever();
-    void listen_forever();
-    char * recv_message(int );
-    void send_message(int , const char *);
+    Communication();
+    void get_server_info();
+
 
 private:
-    std::vector <std::thread> threads;
+
 };
 
 #endif 
