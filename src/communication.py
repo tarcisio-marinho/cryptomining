0df4c305f2865a84234791184ccf7713ee630d00
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-07-05 14:33:12 -0300

    get mining pool info with python and saving into file

--- src/communication.py
+++ src/communication.py
@@ -3,6 +3,9 @@
 
 import urllib.request, json, time
 from sys import argv, exit
+#usage :
+#  python3 communication.py get_pool
+#  python3 communication.py post
 
 # Errors
 # -1 invalid sintaxe
@@ -16,19 +19,27 @@ def offline():
     print("offline")
     exit(-2)
 
-# url = "http://127.0.0.1"
-url = "https://google.com.br"
-
+url = "http://127.0.0.1"
+arq = "mining_pool_info_file.txt"
 
 def get_mining_pool_info():
     try:
         req = urllib.request.urlopen(url).read()
     except urllib.error.URLError:
         offline()
-        
+    
+    
+    List = json.loads(req)
 
-    print(req)
+    with open(arq, 'w') as f:
+        f.write(str(List[0]) + "\n")
+        f.write(str(List[1]))
+    
+    exit(0)
 
+
+def post():
+    pass
 
 if __name__ == "__main__":
     if(len(argv) < 2):
@@ -38,7 +49,7 @@ if __name__ == "__main__":
         get_mining_pool_info()
     
     elif(argv[1] == "post"):
-        pass
+        post()
 
     else:
         error()
